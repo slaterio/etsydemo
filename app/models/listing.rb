f6377af4e3d9e091f3fb9329d5e001f0a25ed4c1
Ben Slater <ben@overmation.com>
2015-04-20 13:01:35 +0100

    associate listings to users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
 		has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg"
+		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	else
 		has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg",
 					  :storage => :dropbox,
@@ -12,4 +13,6 @@ class Listing < ActiveRecord::Base
 	validates :name, :price, :description, presence: true
 	validates :price, numericality: { greater_than: 0 }
 	validates :image, :attachment_presence => true
+
+	belongs_to :user
 end
